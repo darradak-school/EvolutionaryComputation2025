25aeb01699079c53ff0145d58e8906792984e2de
MuradErtas <muradbalzan@gmail.com>
2025-08-14 18:36:11 +0930

    Changed ea test a bit.

--- Assignment1/code/ea_test.py
+++ Assignment1/code/ea_test.py
@@ -1,17 +1,29 @@
 # Basic demo for Evolutionary Algorithm
 from evolutionary import EvolutionaryAlgorithm
 
-print("EA Test")
-print("=" * 20)
+### CONFIGURATION VARIABLES ###
+PROBLEMS = [
+    "eil51",
+    "st70",
+    "eil76",
+    "kroA100",
+    "kroC100",
+    "kroD100",
+    "eil101",
+    "lin105",
+    "pcb442",
+    "pr2392",
+    "usa13509",
+]
+POPULATION = 100
+GENERATIONS = 100
 
 # Create the ea
-ea = EvolutionaryAlgorithm(
-    tsp_file="tsplib/eil51.tsp", population_size=15, max_generations=15
-)
-
-# Run the ea
-print("Running...")
-best = ea.run()
-
-# Show result
-print(f"Done! Best tour length: {best.fitness:.2f}")
+for problem in PROBLEMS:
+    ea = EvolutionaryAlgorithm(
+        f"tsplib/{problem}.tsp", POPULATION, GENERATIONS
+    )
+    print("=" * 20)
+    print(f"Running {problem} with population {POPULATION} and generations {GENERATIONS}")
+    evolve = ea.run()
+    
